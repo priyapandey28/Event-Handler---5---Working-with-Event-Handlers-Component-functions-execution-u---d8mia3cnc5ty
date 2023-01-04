24322242fb8e5e8d99d7efaec247a545c2d9b69b
priyapandey28 <121967472+priyapandey28@users.noreply.github.com>
2023-01-04 23:35:41 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log("form submitted");
+  }
+
+  return (
+    <div id="main">
+      <form onClick={submitHandler}>
+        <label htmlFor='name'>Name</label>
+        <input id="name" type={"text"} />
+
+        <br />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+
+export default App;
